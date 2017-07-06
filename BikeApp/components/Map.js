26cfbc99537dfc4a0172d75d5d57223989fb8ad2
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-06 22:37:49 +0200

    search button and input field are positioned to the bottom of  the screen

--- BikeApp/components/Map.js
+++ BikeApp/components/Map.js
@@ -1,38 +1,101 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Component} from 'react-native';
+import { Alert, AppRegistry, Button, Component, Image ,StyleSheet, TouchableOpacity, Text, TextInput, View} from 'react-native';
 
 import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
+
+  __onPressButton() {
+    Alert.alert('You pressed me!')
+  }
+
   render() {
     return (
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: 47.49801,
-          longitude: 19.03991,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      >
-        <View style={{backgroundColor: '#5414D5', height: 30}}>
-          <TextInput
-            style={{height: 40}}
-            placeholder="Type here the location!"
+      <View style={styles.mainContainer}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 47.49801,
+            longitude: 19.03991,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Type here the location!"
+        />
+        <TouchableOpacity style={styles.searchPictureContainer}>
+          <Image
+            onPress={this._onPressButton}
+            style={styles.searchPictures}
+            source={require('../img/detective.png')}
           />
-        </View>
-      </MapView>
+        </TouchableOpacity>
+        </MapView>
+      </View>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  map: {
+  mainContainer:{
     position: 'absolute',
     top: 80,
     left: 0,
     right: 0,
     bottom: 0,
   },
+  map: {
+    // alignItems: 'flex-end',
+    // position: 'absolute',
+    // top: 0,
+    // left: 0,
+    // right: 0,
+    // height: '100%',
+    // width:'100%',
+    // bottom: 0, 
+    ...StyleSheet.absoluteFillObject,
+  },
+  // searchContainer:{
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // height: 60,
+    // width: '100%',
+    // left: 10,
+    // right: 10,
+    // bottom: 0,
+    // top: 600
+  // },
+  searchPictureContainer:{
+    maxWidth: 55, 
+    maxHeight: 55, 
+    minWidth: 55, 
+    minHeight: 55,
+    margin: 15,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  searchPictures:{
+    maxWidth: 50, 
+    maxHeight: 50, 
+    minWidth: 50, 
+    minHeight: 50,
+    // marginTop: '110%',
+    // position: 'absolute',
+    // right: 10,
+    // bottom: 70,
+    // top: 350,
+  },
+  textInput:{
+    height: 40, 
+    width: 180, 
+    margin: 10,
+    backgroundColor: 'red',
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+  }
 });
