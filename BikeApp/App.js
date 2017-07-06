d57f29cfe28b9aba5d502dc3468c409ab8cda0a1
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-06 13:39:53 +0200

    feat(navbar): created navbar

--- BikeApp/App.js
+++ BikeApp/App.js
@@ -6,15 +6,21 @@ import MapView from 'react-native-maps';
 export default class App extends React.Component {
   render() {
     return (
+        <View style ={styles.container}>
+          <View style ={styles.navbar}>
+            <Text style ={styles.navbarText}>Most</Text>
+          </View>
         <MapView
-          style={ styles.map }
-          initialRegion={{
+          style={styles.map}
+          region={{
             latitude: 47.49801,
             longitude: 19.03991,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
           }}
-        />
+        >
+        </MapView>
+      </View>
     );
   }
 }
@@ -32,9 +38,22 @@ const styles = StyleSheet.create({
   },
   map: {
     position: 'absolute',
-    top: 0,
+    top: 80,
     left: 0,
     right: 0,
     bottom: 0,
   },
+  navbar:{
+    height: 80,
+    width: 900,
+    backgroundColor: 'pink',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  navbarText:{
+    height: 80, 
+    fontSize: 40, 
+    color: 'black'
+  }
 });
