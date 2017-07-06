5e01b2385892b4b0c71c42f587f6ad066c3b3ce1
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-06 16:15:09 +0200

    feat(navbar): added icons to the navbar component

--- BikeApp/components/Navbar.js
+++ BikeApp/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { StyleSheet, Text, View, Component, Image} from 'react-native';
 
 import MapView from 'react-native-maps';
@@ -9,25 +9,66 @@ export default class App extends React.Component {
         <View style ={styles.container}>
           <View style ={styles.navbar}>
             <Image
-              source={require('./img/menu.png')}
+              source={require('../img/menu.png')}
               style={{width: 40, height: 40, margin: 15}}
             />
             <Image
-              source={require('./img/accident.png')}
+              source={require('../img/accident.png')}
               style={{width: 50, height: 50, margin: 10}}
             />
           </View>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 47.49801,
-            longitude: 19.03991,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+})*/
+
+import React from 'react';
+import { StyleSheet, Text, View, Component, Image} from 'react-native';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <View style ={styles.navbar}>
+            <Image
+              source={require('../img/menu.png')}
+              style={styles.menuPictures}
+            />
+            <Image
+              source={require('../img/accident.png')}
+              style={styles.bikePictures}
+            />
+          </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  navbar:{
+    height: 80,
+    width: 900,
+    backgroundColor: '#F5C57D',
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  menuPictures:{
+    marginLeft: 20,
+    marginTop: 30,
+    width: 25, 
+    height: 25, 
+    margin: 10
+  }, 
+  bikePictures:{
+    marginLeft: 20,
+    marginTop: 30,
+    width: 35, 
+    height: 35, 
+    margin: 10
+  }
+  }
+);
