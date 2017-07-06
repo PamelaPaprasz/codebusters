adda12ae0efb94d5481d97e89a2e0de2a7cc8e74
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-06 13:14:05 +0200

    budpaest is on the map

--- BikeApp/App.js
+++ BikeApp/App.js
@@ -6,32 +6,19 @@ import MapView from 'react-native-maps';
 export default class App extends React.Component {
   render() {
     return (
-      // <View style={styles.container}>
-      //   <Text>Open up App.js to start working on your app!</Text>
-      //   <Text>Changes you make will automatically reload.</Text>
-      //   <Text>Shake your phone to open the developer menu.</Text>
-      // </View>
-      <MapView
-        style={ styles.map }
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+        <MapView
+          style={ styles.map }
+          initialRegion={{
+            latitude: 47.49801,
+            longitude: 19.03991,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 const styles = StyleSheet.create({
   container: {
